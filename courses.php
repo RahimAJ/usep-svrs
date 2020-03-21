320ab56ebedf23a9efd3c0e16d578b6bca23b9b0
rahimAJ <rahimjumlani@gmail.com>
2020-03-21 22:38:00 +0800

    Data tables are all functional, clicking row will give out the data id.

--- courses.php
+++ courses.php
@@ -1,8 +1,52 @@
 <?php include('templates/header.php') ?>
 <?php include('templates/navbar.php') ?>
 
+<?php
+include('includes/dbh-inc.php');
+$violations_array = mysqli_query($conn, "SELECT * FROM courses");
+?>
+
 <div class="container mt-4">
-    This is the Courses page
+    <table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+            <tr>
+                <th>Course ID</th>
+                <th>Course Name</th>
+                <th>No. of Students</th>
+                <th>No. of Violations</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($row = mysqli_fetch_array($violations_array)) {?>
+                <tr onclick="window.location='courses.php?course-id=<?php echo $row['id']; ?>'">
+                    <td><?php echo $row['course_id']; ?></td>
+                    <td><?php echo $row['course_name']; ?></td>
+                    <td>
+                        <?php
+                        $course_id = $row['course_id'];
+                        $result = mysqli_query($conn, "SELECT * FROM students WHERE course_id = '$course_id';");
+                        echo mysqli_num_rows($result);
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                        $course_id = $row['course_id'];
+                        $result = mysqli_query($conn, "SELECT violations.student_id FROM violations
+                        LEFT JOIN students ON students.student_id = violations.student_id
+                        WHERE students.course_id = '$course_id';");
+                        echo mysqli_num_rows($result);
+                        ?>
+                    </td>
+                </tr>
+            <?php
+            } ?>
+        </tbody>
+    </table>
 </div>
 
 <?php include('templates/footer.php') ?>
+<script>
+    $(document).ready(function() {
+        $('#example').DataTable();
+    });
+</script>
--- student-list.php
+++ student-list.php
@@ -1,8 +1,40 @@
 <?php include('templates/header.php') ?>
 <?php include('templates/navbar.php') ?>
 
+<?php
+include('includes/dbh-inc.php');
+$violations_array = mysqli_query($conn, "SELECT * FROM students");
+?>
+
 <div class="container mt-4">
-  This is the Student List page
+  <table id="example" class="table table-striped table-bordered" style="width:100%">
+    <thead>
+      <tr>
+        <th>Student ID</th>
+        <th>Lastname</th>
+        <th>Firstname</th>
+        <th>Course</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php while ($row = mysqli_fetch_array($violations_array)) {
+        if ($row['deleted'] != NULL)
+          continue; ?>
+        <tr onclick="window.location='student-list.php?student-id=<?php echo $row['id']; ?>'">
+          <td><?php echo $row['student_id']; ?></td>
+          <td><?php echo $row['student_lastname']; ?></td>
+          <td><?php echo $row['student_firstname']; ?></td>
+          <td><?php echo $row['course_id']; ?></td>
+        </tr>
+      <?php
+      } ?>
+    </tbody>
+  </table>
 </div>
 
 <?php include('templates/footer.php') ?>
+<script>
+  $(document).ready(function() {
+    $('#example').DataTable();
+  });
+</script>
